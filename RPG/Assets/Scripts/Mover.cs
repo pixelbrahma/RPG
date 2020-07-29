12d5e9c3c9ff0e34220124fcf9ce58a3555ef436
Nikhil Bharadwaj <bharadwajnikhil77@gmail.com>
2020-07-29 10:49:22 +0530

    nav mesh changes

---
+++ RPG/Assets/Scripts/Mover.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine.AI;
+using UnityEngine;
+
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+
+    private void Update()
+    {
+        GetComponent<NavMeshAgent>().destination = target.position;
+    }
+}

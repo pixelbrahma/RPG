84a6fb75aac4f89cc200f00d2542aa50b43f282a
Nikhil Bharadwaj <bharadwajnikhil77@gmail.com>
2020-07-29 14:29:36 +0530

    Movement and folder structure organisation

---
+++ RPG/Assets/Scripts/Movement/Mover.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine.AI;
+using UnityEngine;
+
+
+public class Mover : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+            MoveToCursor();
+        UpdateAnimator();
+    }
+
+    private void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+            GetComponent<NavMeshAgent>().destination = hit.point;
+    }
+
+    private void UpdateAnimator()
+    {
+        Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        float speed = localVelocity.z;
+        GetComponent<Animator>().SetFloat("ForwardSpeed", speed);
+    }
+}

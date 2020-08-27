8f6d2da3e9ffe8fcc808972bdae0a7b6ad0b5307
Nikhil Bharadwaj <bharadwajnikhil77@gmail.com>
2020-08-27 13:58:47 +0530

    AI Basic Logic

---
+++ RPG/Assets/Scripts/Control/AIController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Control
+{
+    public class AIController : MonoBehaviour
+    {
+        [SerializeField] private float chaseDistance = 5f;
+        private GameObject player;
+
+        private void Start()
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+        }
+
+        private void Update()
+        {
+            if (DistanceToPlayer() <= chaseDistance)
+                Debug.Log("Enemy : " + gameObject.name + " should chase");
+        }
+
+        private float DistanceToPlayer()
+        {
+            return Vector3.Distance(player.transform.position, transform.position);
+        }
+    }
+}
